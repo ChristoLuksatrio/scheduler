9068176c5759146e9c2feaa0a2f9598dad24d524
ChristoLuksatrio <christo.luksatrio@gmail.com>
2020-02-26 15:41:58 -0500

    add all tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -21,4 +21,31 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
+  it("should edit an interview", () => {
+    cy.get('[alt=Edit]')
+      .click({ force: true })
+    cy.get(':nth-child(2) > .interviewers__item-image')
+      .click()
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type('John Doe')
+    cy.get('.button--confirm')
+      .click()
+    cy.contains(".appointment__card--show", "John Doe");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get('[alt=Delete]')
+      .click({ force: true })
+    cy.get('.appointment__actions > :nth-child(2)')
+      .click()
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+  
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
+
+
 });
--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,60 +4,60 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
 
-  switch (action.type) {
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.value
-       }
-    case SET_APPLICATION_DATA:
-      return { 
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-       }
-    case SET_INTERVIEW: {
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: { ...action.interview }
-      };
-
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment
-      };
-
-      const { id, interview } = action
-
-
-      return {
-        ...state,
-        appointments,
-        days: state.days.map(day => {
-          let spotCheck = 0;
-
-          if (day.name === state.day) {
-            if (interview && state.appointments[id].interview) {
-              spotCheck = 0;
-            } else if (interview) {
-              spotCheck = -1;
-            } else {
-              spotCheck = 1;
+    switch (action.type) {
+        case SET_DAY: return {
+                ... state,
+                day: action.value
             }
-          }
+        case SET_APPLICATION_DATA: return {
+                ... state,
+                days: action.days,
+                appointments: action.appointments,
+                interviewers: action.interviewers
+            }
+        case SET_INTERVIEW: {
+                const appointment = {
+                    ... state.appointments[action.id],
+                    interview: {
+                        ... action.interview
+                    }
+                };
 
-          return {
-            ...day,
-            spots: day.spots + spotCheck
-          }
+                const appointments = {
+                    ... state.appointments,
+                    [action.id]: appointment
+                };
 
-        })
-      }
+                const {id, interview} = action
+
+
+                return {
+                    ... state,
+                    appointments,
+                    days: state.days.map(day => {
+                        let spotCheck = 0;
+
+                        if (day.name === state.day) {
+                            if (interview && state.appointments[id].interview) {
+                                spotCheck = 0;
+                            } else if (interview) {
+                                spotCheck = -1;
+                            } else {
+                                spotCheck = 1;
+                            }
+                        }
+
+                        return {
+                            ...day,
+                            spots: day.spots + spotCheck
+                        }
+
+                    })
+                }
+            }
+        default:
+            throw new Error(`Tried to reduce with unsupported action type: ${
+                action.type
+            }`);
     }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
 }
